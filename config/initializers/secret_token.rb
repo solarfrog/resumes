6c9ef94ca29db2cb2f7d1195252196fe107b5bcd
Lance Leonard <lance.leonard@solarfrog.com>
2013-02-22 00:09:03 -0600

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Resumes::Application.config.secret_token = '8e77b76fdecd04ba9a5016413af6f2d1bd8b2c0506f711fdd0f6291b78703f75e9594bca5687de67e8bc5f5ebf2839c14eb9daae4df127affa3bedf0374eb2be'
